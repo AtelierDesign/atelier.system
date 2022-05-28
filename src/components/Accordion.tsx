0388c0ed702ca683a2e07452c23e5aadaabe3954
chvndler <70621588+chvndler@users.noreply.github.com>
2022-05-28 10:36:49 -0400

    fgfg

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -18,49 +18,76 @@ export const Accordion = React.forwardRef<React.ElementRef<typeof StyledAccordio
 
 const StyledItem = styled(AccordionPrimitive.Item, {
   borderTop: '1px solid $colors$slate6',
+  overflow: 'hidden',
+  backgroundColor: '$slate1',
 
+  '&:first-child': {
+    borderTopLeftRadius: '16px',
+    borderTopRightRadius: '16px',
+  },
+  '&:last-child': {
+    borderBottomLeftRadius: '16px',
+    borderBottomRightRadius: '16px',
+  },
+  '&:focus-within': {
+    position: 'relative',
+    zIndex: 1,
+    boxShadow: '0 0 0 2px $mauve12',
+  },
   '&:last-of-type': {
     borderBottom: '1px solid $colors$slate6',
   },
 });
 
 const StyledHeader = styled(AccordionPrimitive.Header, {
+  paddingTop: '3px',
+  borderRadius: '16px',
   all: 'unset',
+  display: 'flex',
 });
 
 const StyledTrigger = styled(AccordionPrimitive.Trigger, {
   all: 'unset',
   boxSizing: 'border-box',
   userSelect: 'none',
+  fontFamily: '$inter',
+  paddingTop: '0px',
+  paddingBottom: '0px',
+  paddingLeft: '20px',
+  paddingRight: '20px',
+  height: '45px',
+  flex: '1',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  fontSize: '13px',
+  fontWeight: '700',
+  lineHeight: '1',
+  color: '$gray12',
+  width: '100%',
+
+  '&[data-state="closed"]': { backgroundColor: '$slate1' },
+  '&[data-state="open"]': { backgroundColor: '$slate3' },
+  '&:hover': { backgroundColor: '$slate5' },
+
   '&::before': {
     boxSizing: 'border-box',
   },
   '&::after': {
     boxSizing: 'border-box',
   },
-
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  p: '$2',
-  color: '$hiContrast',
-  width: '100%',
-
   '@hover': {
     '&:hover': {
       backgroundColor: '$slate2',
     },
   },
-
   '&:focus': {
     outline: 'none',
     boxShadow: 'inset 0 0 0 1px $colors$slate8, 0 0 0 1px $colors$slate8',
   },
-
   svg: {
     transition: 'transform 175ms cubic-bezier(0.65, 0, 0.35, 1)',
   },
-
   '&[data-state="open"]': {
     svg: {
       transform: 'rotate(180deg)',
